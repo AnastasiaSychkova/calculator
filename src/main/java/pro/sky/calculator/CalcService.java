04e4172f733bdedd5256506c549fbcf97073b8ad
Anastasia <sychkova.nastyusha@mail.ru>
2023-02-24 16:06:59 +0400

    hw done

--- src/main/java/pro/sky/calculator/CalcService.java
+++ src/main/java/pro/sky/calculator/CalcService.java
@@ -7,9 +7,20 @@ public class CalcService implements CalcServiceInterface{
         return "Добро пожаловать в калькулятор";
     }
     public String calculate(int num1, int num2){
-        num2 = 5;
-        num1 = 5;
         int i = num1+num2;
         return num1 + " + " + num2 + " = " + i;
+    }
+    public String calculateMin(int num1, int num2){
+        int i = num1-num2;
+        return num1 + " - " + num2 + " = " + i;
+    }
+    public String calculateMultiply(int num1, int num2){
+        int i = num1*num2;
+        return num1 + " * " + num2 + " = " + i;
+    }
+    public String calculateDivide(int num1, int num2){
+        if(num2 == 0) return "ошибка";
+        int i = num1/num2;
+        return num1 + " / " + num2 + " = " + i;
     }
 }
--- src/main/java/pro/sky/calculator/CalcServiceInterface.java
+++ src/main/java/pro/sky/calculator/CalcServiceInterface.java
@@ -3,4 +3,7 @@ package pro.sky.calculator;
 public interface CalcServiceInterface {
     String calc();
     String calculate(int num1, int num2);
+    String calculateMin(int num1, int num2);
+    String calculateMultiply(int num1, int num2);
+    String calculateDivide(int num1, int num2);
 }
